a2fa63676c75ecaf82fa11843c9e3935e12375bb
guangcheng.zhang <guangcheng.zhang@shanbay.com>
2018-09-21 14:17:57 +0800

    fill the input by select file

--- index.js
+++ index.js
@@ -109,6 +109,104 @@ var app = new Vue({
             }
             return "#" + hex;
         },
+
+        importFile() {
+            var selectedFile = document.getElementById("files").files[0]; //获取读取的File对象
+            var name = selectedFile.name; //读取选中文件的文件名
+            var size = selectedFile.size; //读取选中文件的大小
+
+            var reader = new FileReader(); //这里是核心！！！读取操作就是由它完成的。
+            reader.readAsText(selectedFile); //读取文件的内容('xml-formatter');
+            var that = this
+            reader.onload = function () {
+                // console.log(this.result); //当读取完成之后会回调这个函数，然后此时文件的内容存储到了result中。直接操作即可。
+                var xmlDoc = that.xmlParser(this.result)
+                that.fillData(xmlDoc)
+            };
+        },
+
+        xmlParser(xmlString) {
+            var xmlDoc = null;
+            //判断浏览器的类型
+            //支持IE浏览器 
+            if (!window.DOMParser && window.ActiveXObject) { //window.DOMParser 判断是否是非ie浏览器
+                var xmlDomVersions = ['MSXML.2.DOMDocument.6.0', 'MSXML.2.DOMDocument.3.0', 'Microsoft.XMLDOM'];
+                for (var i = 0; i < xmlDomVersions.length; i++) {
+                    try {
+                        xmlDoc = new ActiveXObject(xmlDomVersions[i]);
+                        xmlDoc.async = false;
+                        xmlDoc.loadXML(xmlString); //loadXML方法载入xml字符串
+                        break;
+                    } catch (e) {}
+                }
+            }
+            //支持Mozilla浏览器
+            else if (window.DOMParser && document.implementation && document.implementation.createDocument) {
+                try {
+                    /* DOMParser 对象解析 XML 文本并返回一个 XML Document 对象。
+                     * 要使用 DOMParser，使用不带参数的构造函数来实例化它，然后调用其 parseFromString() 方法
+                     * parseFromString(text, contentType) 参数text:要解析的 XML 标记 参数contentType文本的内容类型
+                     * 可能是 "text/xml" 、"application/xml" 或 "application/xhtml+xml" 中的一个。注意，不支持 "text/html"。
+                     */
+                    domParser = new DOMParser();
+                    xmlDoc = domParser.parseFromString(xmlString, 'text/xml');
+                } catch (e) {
+                    console.log(e);
+                }
+            } else {
+                return null;
+            }
+
+            return xmlDoc;
+        },
+
+        // 解析选中 xml 文件 填充到 data 数据中
+        fillData(xmlDoc) {
+            for (themeKey in this.theme) {
+                if (themeKey === 'read_page') {
+                    var read_page_child = xmlDoc.getElementsByTagName("read_page")[0].childNodes
+                    // console.log(read_page_child);
+                    const length = read_page_child.length
+                    for (let index = 0; index < length; index++) {
+                        const element = read_page_child[index];
+                        // console.log(element.nodeName)
+                        // var items = this.theme.read_page[element.nodeName]
+                        // console.log(element.childNodes)
+                        if (element.nodeType === 3 || element.nodeType === 8) { //过滤注释
+                            continue
+                        }
+
+                        const nodeName = element.nodeName;
+                        const childNodes = element.childNodes
+                        var items = this.theme.read_page[nodeName]
+                        items.forEach(item => {
+                            for (let index = 0; index < childNodes.length; index++) {
+                                const element2 = childNodes[index];
+                                const childNodeName = element2.nodeName
+                                if (element2.nodeType === 3 || element2.nodeType === 8) { //过滤注释
+                                    continue
+                                }
+                                if (item.tag === childNodeName) {
+                                    item.text = element2.childNodes[0].nodeValue
+                                }
+                            }
+                        })
+                    }
+                } else {
+                    this.theme[themeKey].forEach(element => {
+                        var childItems = xmlDoc.getElementsByTagName(themeKey)[0].childNodes;
+                        childItems.forEach(item => {
+                            if (item.nodeType === 3 || item.nodeType === 8) {
+                                return
+                            }
+                            if (element.tag === item.nodeName) {
+                                element.text = item.childNodes[0].nodeValue
+                            }
+                        })
+                    });
+                }
+            }
+        },
     },
     data: {
         theme: {
@@ -168,35 +266,21 @@ var app = new Vue({
                         type: 'color',
                         tag: 'text_color',
                         text: 'ff333333',
-                        tip: '目录字体颜色'
-                    },
-                    {
-                        id: 9,
-                        type: 'color',
-                        tag: 'catalog_title_icon_color',
-                        text: 'ffa1660c',
-                        tip: '目录图标颜色'
-                    },
-                    {
-                        id: 10,
-                        type: 'color',
-                        tag: 'catalog_dividing_line_color',
-                        text: 'e5816130',
-                        tip: '目录分割线颜色'
+                        tip: '目录常规字体颜色'
                     },
                     {
                         id: 11,
                         type: 'color',
                         tag: 'catalog_item_selected_bg_color',
                         text: 'fffcedc2',
-                        tip: '目录被选中颜色'
+                        tip: '目录被选中背景颜色'
                     },
                     {
                         id: 12,
                         type: 'color',
                         tag: 'catalog_item_finished_icon_color',
                         text: 'ffa1660c',
-                        tip: '目录完成图标'
+                        tip: '目录完成图标颜色'
                     },
                     {
                         id: 13,
@@ -210,15 +294,29 @@ var app = new Vue({
                         type: 'color',
                         tag: 'catalog_item_locked_icon_color',
                         text: '80979797',
-                        tip: '目录未解锁图标颜色'
+                        tip: '未解锁目录图标颜色'
                     },
                     {
                         id: 15,
                         type: 'color',
-                        tag: 'locked_text_color',
+                        tag: 'catalog_item_locked_text_color',
                         text: '99333333',
-                        tip: '目录未解锁字体颜色'
+                        tip: '未解锁目录字体颜色'
                     },
+                    {
+                        id: 16,
+                        type: 'color',
+                        tag: 'catalog_item_selected_text_color',
+                        text: '99333333',
+                        tip: '目录被选中字体颜色'
+                    },
+                    {
+                        id: 17,
+                        type: 'color',
+                        tag: 'catalog_item_length_text_color',
+                        text: '99333333',
+                        tip: '目录章节字数字体颜色'
+                    }
                 ],
                 body: [{
                         id: 1,
@@ -589,13 +687,6 @@ var app = new Vue({
                     tag: 'thumb_color',
                     text: 'ffedd599',
                     tip: '滚动条颜色'
-                },
-                {
-                    id: 5,
-                    type: 'color',
-                    tag: 'divider_color',
-                    text: '59e5c982',
-                    tip: '弹窗中购买部分与内容部分分割线颜色'
                 },
                 {
                     id: 6,
